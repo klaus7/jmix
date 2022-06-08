891fa2445997c11ebeba7243e1048ca73d42e0d4
Roman Pinyazhin <pinyazhin@haulmont.com>
2022-06-08 16:05:28 +0400

    FLow UI project template

--- jmix-templates/content/project/flowui-single-module-application/src/main/java/${project_rootPath}/screen/login/LoginScreen.java
+++ jmix-templates/content/project/flowui-single-module-application/src/main/java/${project_rootPath}/screen/login/LoginScreen.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2022 Haulmont.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package ${project_rootPackage}.screen.login;
 
 import com.vaadin.flow.component.login.AbstractLogin.LoginEvent;
@@ -32,12 +16,12 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.LockedException;
 
-@UiController("${normalizedPrefix_underscore}login-screen")
+@UiController("${normalizedPrefix_underscore}LoginScreen")
 @UiDescriptor("login-screen.xml")
 @Route(value = "login")
-public class LoginView extends StandardScreen {
+public class LoginScreen extends StandardScreen {
 
-    private final Logger log = LoggerFactory.getLogger(LoginView.class);
+    private final Logger log = LoggerFactory.getLogger(LoginScreen.class);
 
     @Autowired
     private LoginScreenSupport loginScreenSupport;
--- jmix-templates/content/project/flowui-single-module-application/src/main/java/${project_rootPath}/screen/user/UserBrowse.java
+++ jmix-templates/content/project/flowui-single-module-application/src/main/java/${project_rootPath}/screen/user/UserBrowse.java
@@ -1,13 +1,14 @@
-package com.company.demo.screen.user;
+package ${project_rootPackage}.screen.user;
 
-import ${project_rootPackage}.screen.main.MainSceen;
+import ${project_rootPackage}.entity.User;
+import ${project_rootPackage}.screen.main.MainScreen;
 import com.vaadin.flow.router.Route;
 import io.jmix.flowui.screen.*;
 
 @UiController("${normalizedPrefix_underscore}User.browse")
 @UiDescriptor("user-browse.xml")
 @LookupComponent("usersTable")
-@Route(value = UserBrowse.ROUTE, layout = MainSceen.class)
+@Route(value = UserBrowse.ROUTE, layout = MainScreen.class)
 public class UserBrowse extends StandardLookup<User> {
     public static final String ROUTE = "users";
 }
